44f165345b6410e2f877f3befa9879aaccf279ff
Ivan Gomez <oigomezz@unal.edu.co>
2020-10-25 21:18:20 -0500

    Add components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render() {
+        return (
+            <footer>
+                <small>
+                    Cetaphil 70 años                    
+                </small>
+                
+            </footer>
+        )
+    }
+}
+
+export default Footer
---
+++ src/data/data.js
@@ -0,0 +1,63 @@
+export const data = [
+    {
+        question: '¿La textura de mi piel es?',
+        answers: ['Escamosa','Gruesa','Aspera','Suave y lisa'],
+        correct: [1,2,3,4]
+    },
+
+    {
+        question: 'Where in an HTML document is the correct place to refer to an external style sheet?',
+        answers: ['Si,siempre he tenido granos y espinillas pero más en la zona t (frente,nariz,mentón)','He tenido granos y espenillas en toda la cara','Nunca he sufrido de acné en mi rostro','Si pero no tan frecuente'],
+        correct: [1,2,3,4]
+    },    
+    {
+        question: 'Which HTML tag is used to define an internal style sheet?',
+        answers: ['<style>', '<script>', '<headStyle>', '<css>'],
+        correct: [1,2,3,4]
+    },    
+    {
+        question: 'Which HTML attribute is used to define inline styles?',
+        answers: ['class', 'style', 'font', 'styles'],
+        correct: [1,2,3,4]
+    },
+    {
+        question: 'Which is the correct CSS syntax?',
+        answers: ['{body:color=black;}', '{body;color:black;}', 'body:color=black;', 'body {color: black;}'],
+        correct: [1,2,3,4]
+    },
+    {
+        question: 'How do you insert a comment in a CSS file?',
+        answers: ['\' this is a comment', '/* this is a comment */', '// this is a comment', '// this is a comment //'],
+        correct: [1,2,3,4]
+    },
+    {
+        question: 'Which property is used to change the background color?',
+        answers: ['color', 'bgcolor', 'background-color', 'bgColor'],
+        correct: [1,1,0,0]
+    },
+    {
+        question: 'How do you add a background color for all <h1> elements?',
+        answers: ['all.h1 {background-color:#FFFFFF;}', 'h1.setAll {background-color:#FFFFFF;}', 'h1.all {background-color:#FFFFFF;}', 'h1 {background-color:#FFFFFF;}'],
+        correct: [1,1,0,0]
+    },
+    {
+        question: '¿Consideras que tu piel es tirante y luce opaca?',
+        answers: ['En rostro', 'h1.setAll {background-color:#FFFFFF;}', 'h1.all {background-color:#FFFFFF;}', 'h1 {background-color:#FFFFFF;}'],
+        correct: [1,1,0,0]
+    },
+    {
+        question: '¿Ha sufrido alguna vez de irritación en la piel?',
+        answers: ['Si, es muy frecuente', 'No es tan frecuente pero en varias ocaciones se me ha irritado la piel', 'Nunca', 'Han sido muy pocas veces'],
+        correct1: [1,1,0,0]
+    },
+    {
+        question: '¿Sufres de alergias y enrojecimiento en la piel?',
+        answers: ['Mi piel se enrojese con cualquier cosa', 'Si me ocurre pero no es tan frecuente', 'Por fortuna no', 'Ha sucedido tan poco que no lo concidero relevante'],
+        correct1: [1,1,0,0]
+    },
+    {
+        question: '¿Frente a los cambios de temperatura mi piel?',
+        answers: ['Se enrojese con facilidad', 'Cambia de aspecto se vuelve más grasosa o por el contrario, la siento reseca', 'Mi piel sigue igual', 'No he notado ningun cambio'],
+        correct1: [1,1,0,0]
+    }
+]
