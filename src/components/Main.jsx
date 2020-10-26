928b1e37564f7bedc636cc5696f12becef0c9424
Ivan Gomez <oigomezz@unal.edu.co>
2020-10-25 23:18:38 -0500

    Add button and image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,43 +1,30 @@
 import React, { Component } from 'react';
-import { data } from '../data/data';
 import Header from './Header';
 import Footer from './Footer';
 import img from '../logo.svg';
 
 export default class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            nr: 0,
-            total: data.length,
-            showButton: false,
-            questionAnswered: false,
-            score: 0,
-            displayPopup: 'flex'
-        }
-    }
-
+    
     render() {
-        //let { total, question, showButton} = this.state;
         
         return (
             <React.Fragment>    
                 <Header/>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-6 ">
-                            <div className="contenedor">
+                {
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-6">
                                 <div className="contenido">        
                                     <p>¿Cual es mi tipo de piel y que producto debo utilizar?</p>
                                     <button>Empezar el test ahora</button>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-6">
-                            <img className="Image" src={img} alt="Imagen"></img>
+                            <div className="col-6">
+                                <img className="Image contenido" src={img} alt="Imagen"></img>
+                            </div>
                         </div>
                     </div>
-                </div>
+                }
                 <Footer/>
             </React.Fragment>
         );
