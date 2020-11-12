80805bc927e6319d627469e34f4425f16f4ac56a
Miguel Rivera <mariveran@unal.edu.co>
2020-11-11 21:18:57 -0500

    initial slide

--- src/components/Result/Main.jsx
+++ src/components/Result/Main.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import SimpleSlider from './Slider';
 
 export default class Main extends Component {
     render() {
         return(
-            <div>
+            <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-4">
                         <h3 style={{color:'#627892',textAlign:'center'}}>Los productos que te recomendamos para tu rutina de cuidado diario son:</h3> 
@@ -13,11 +14,11 @@ export default class Main extends Component {
                 <div className="row justify-content-center">
                     <p>Aqui iria el slider ese culero :v</p>
                 </div>
-                <div className="row justify-content-center">
-                    <p>Aqui estaria el carrusel de la contratacion... espera khe?</p>
+                <div className="justify-content-center">
+                    <SimpleSlider/>
                 </div>
                 <div className="row justify-content-center">
-                    <Link className="btn btn-light" style={{margin:'20px', width:'auto', backgroundColor:'#62788D', color:'#FFFFFF'}}>Descarga aquí tu resultado</Link>
+                    <Link className="btn btn-light" to="/home" style={{margin:'20px', width:'auto', backgroundColor:'#62788D', color:'#FFFFFF'}}>Descarga aquí tu resultado</Link>
                 </div>
             </div>
         );
---
+++ src/components/Result/Slider.jsx
@@ -0,0 +1,81 @@
+import React,  { Component } from 'react';
+import Carousel from "react-multi-carousel";
+
+import Producto1 from '../../img/Producto1.png'
+import Producto2 from '../../img/Producto2.png'
+import Producto3 from '../../img/Producto3.png'
+import Producto4 from '../../img/Producto4.png'
+import Producto5 from '../../img/Producto5.png'
+import Producto6 from '../../img/Producto6.png'
+
+import "react-multi-carousel/lib/styles.css";
+
+export default class SimpleSlider extends Component {
+    render(){
+        const responsive = {
+            desktop: {
+                breakpoint: { max: 3000, min: 1024 },
+                items: 3,
+                slidesToSlide: 3 // optional, default to 1.
+              },
+              tablet: {
+                breakpoint: { max: 1024, min: 464 },
+                items: 2,
+                slidesToSlide: 2 // optional, default to 1.
+              },
+              mobile: {
+                breakpoint: { max: 464, min: 0 },
+                items: 1,
+                slidesToSlide: 1 // optional, default to 1.
+              }
+        };
+        return(
+            <div className="container">
+                <Carousel
+                    arrows={true}
+                    showDots={false}
+                    responsive={responsive}
+                    ssr={true} // means to render carousel on server-side.
+                    infinite={true}
+                    customTransition="all .4"
+                    transitionDuration={500}
+                    containerClass="carousel-container"
+                    removeArrowOnDeviceType={["tablet", "mobile"]}
+                    deviceType={this.props.deviceType}
+                    dotListClass="custom-dot-list-style"
+                    itemClass="carousel-item-padding-40-px"
+                    centerMode={false}
+                    renderDotsOutside={false}
+                    >
+                    <div style={{border:"solid 1px",borderColor:"black"}}>
+                        <img src={Producto1} alt=""/>
+                    </div>
+                    <div>
+                        <img src={Producto2} alt=""/>
+                    </div>
+                    <div>
+                        <img src={Producto3} alt=""/>
+                    </div>
+                    <div>
+                        <img src={Producto4} alt="" />
+                    </div>
+                    <div>
+                        
+                    </div>
+                    <div>
+                        <img src={Producto5} alt=""/>
+                    </div>
+                    <div>
+                        
+                    </div>
+                    <div>
+                        
+                    </div>
+                    <div>
+                        <img src={Producto6} alt=""/>
+                    </div>
+                </Carousel>
+            </div>
+        );
+    }
+}
