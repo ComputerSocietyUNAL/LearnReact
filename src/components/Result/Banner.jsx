d3ea2adc77172fbaa43af91077286e20261e3faf
Miguel Rivera <mariveran@unal.edu.co>
2020-11-18 15:32:21 -0500

    try manual merge part1

--- src/components/Result/Banner.jsx
+++ src/components/Result/Banner.jsx
@@ -6,29 +6,33 @@ export default class Banner extends Component {
     render(){
         return(
             <div className="Banner" >
-                <div className="row justify-content-center" style={{margin:"0px"}}>
-                    <div className="col-4 box lg" >
-                        <img src="/assets/BannerIZQ.png" className="Banner-img" alt=""/>
-                    </div>
-                    {   this.props.sensibleSkinScore > 1 ?
-                        <div className="col-4 Banner-text">
-                            <p>Conoce tu tipo de piel y su cuidado especializado</p>
-                            <Button color="secondary" onClick={e => this.props.changeSensibleScore(e)}>Ver más</Button>
+                <div className="container text-center">
+                    <div className="row">
+                        <div className="col-md-4 box lg" >
+                            <img src="/assets/BannerIZQ.png" className="Banner-img" alt=""/>
                         </div>
-                        :
-                        <div className="col-5 col-sm Banner-text">
-                            <p>Conoce tu tipo de piel y su cuidado especializado</p>
-                            <Link 
-                            className="btn btn-light" to="/test" 
-                            style={{
-                                margin:'auto', 
-                                width:'auto', 
-                                backgroundColor:"#62788D",
-                                color:"white"}}>Rehacer Test</Link>
+                        <div className="col-md-4">
+                        {   this.props.sensibleSkinScore > 1 ?
+                            <div className="Banner-text">
+                                <p>Conoce tu tipo de piel y su cuidado especializado</p>
+                                <Button color="secondary" className="animate__animated animate__pulse animate__infinite Banner-button" onClick={e => this.props.changeSensibleScore(e)}>Ver más</Button>
+                            </div>
+                            :
+                            <div className="Banner-text">
+                                <p>Conoce tu tipo de piel y su cuidado especializado</p>
+                                <Link 
+                                className="btn btn-light animate__animated animate__pulse animate__infinite " to="/test" 
+                                style={{
+                                    margin:'15px', 
+                                    width:'auto', 
+                                    backgroundColor:"#62788D",
+                                    color:"white"}}>Rehacer Test</Link>
+                            </div>
+                        }
                         </div>
-                    }
-                    <div className="col-4 box lg" >
-                        <img src="/assets/BannerDER.png" className="Banner-img" alt=""/>
+                        <div className="col-md-4 box lg" >
+                            <img src="/assets/BannerDER.png" className="Banner-img" alt=""/>
+                        </div>
                     </div>
                 </div>
             </div>
